65b9ad9d8d769cc3f22557ffbef7c406053d45cd
TanguyJeanneau <tanguy.jeanneau@gmail.com>
2024-06-04 13:37:02 +0000

    Adapt IrisEncoder unit tests

--- tests/e2e_tests/nodes/encoder/test_e2e_iris_encoder.py
+++ tests/e2e_tests/nodes/encoder/test_e2e_iris_encoder.py
@@ -24,6 +24,7 @@ def test_iris_encoder_constructor() -> None:
 
     assert len(result.iris_codes) == len(expected_result.iris_codes)
     assert len(result.mask_codes) == len(expected_result.mask_codes)
+    assert len(result.iris_code_version) == len(expected_result.iris_code_version)
 
     for i, (i_iris_code, i_mask_code) in enumerate(zip(result.iris_codes, result.mask_codes)):
         assert np.allclose(expected_result.iris_codes[i], i_iris_code, rtol=1e-05, atol=1e-07)
