44913cffef2f4d8e0acb7439f1aa8c6f3d4f572b
ycbiometrics <yi.chen@ip-10-54-144-12.eu-central-1.compute.internal>
2025-05-15 21:14:52 +0000

    update unit tests

--- tests/e2e_tests/nodes/iris_response_refinement/test_e2e_fragile_bits_refinement.py
+++ tests/e2e_tests/nodes/iris_response_refinement/test_e2e_fragile_bits_refinement.py
@@ -18,21 +18,23 @@ def load_mock_pickle(name: str) -> Any:
 
 
 @pytest.mark.parametrize(
-    "value_threshold,fragile_type",
+    "value_threshold,fragile_type,maskisduplicated",
     [
-        pytest.param([0, 0.5, 0.5], FragileType.cartesian),
-        pytest.param([0, 0.49, np.pi / 8], FragileType.polar),
+        pytest.param([0, 0.5, 0.5], FragileType.cartesian, False),
+        pytest.param([0, 0.49, np.pi / 8], FragileType.polar, False),
+        pytest.param([0, 0.5, 0.5], FragileType.cartesian, True),
+        pytest.param([0, 0.49, np.pi / 8], FragileType.polar, True),
     ],
-    ids=["cartesian", "polar"],
+    ids=["cartesian", "polar", "cartesian_maskisduplicated", "polar_maskisduplicated"],
 )
 def test_fragile_bits_dummy_responses(
-    value_threshold: Tuple[confloat(ge=0), confloat(ge=0), confloat(ge=0)], fragile_type: FragileType
+    value_threshold: Tuple[confloat(ge=0), confloat(ge=0), confloat(ge=0)], fragile_type: FragileType, maskisduplicated: bool,
 ) -> None:
     iris_filter_response = load_mock_pickle(f"artificial_iris_responses_{fragile_type}")
 
-    mask_responses_refined = load_mock_pickle(f"artificial_mask_responses_{fragile_type}_expected_refinement")
+    mask_responses_refined = load_mock_pickle(f"artificial_mask_responses_{fragile_type}_{int(maskisduplicated)}_expected_refinement")
 
-    fragile_bit_refinement = FragileBitRefinement(value_threshold=value_threshold, fragile_type=fragile_type)
+    fragile_bit_refinement = FragileBitRefinement(value_threshold=value_threshold, fragile_type=fragile_type, maskisduplicated=maskisduplicated)
     iris_filter_response_refined = fragile_bit_refinement(iris_filter_response)
 
     for refined_mask, obtained_mask in zip(mask_responses_refined, iris_filter_response_refined.mask_responses):
